b938ff20a8fbddd240f6b5abe9ae629cd4459dbf
Wes Sitzes <polybiuspro@protonmail.com>
2025-02-13 13:28:24 -0500

    feat: config multer and add file analyze route
    feat: config multer to store in memory
    
    feat: create file analyze route

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const cors = require("cors");
 const multer = require("multer");
 require("dotenv").config();
 
+
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage});
+
 var app = express();
 
 app.use(cors());
@@ -11,6 +15,15 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.get("/", function (req, res) {
     res.sendFile(process.cwd() + "/views/index.html");
 });
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+    const file = req.file;
+    res.json({
+        name: file.originalname,
+        type: file.mimetype,
+        size: file.size
+    })
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
